a724183ee94edc4510541f7cfb0797d1d2dea634
shubendu <biswasshubendu4@gmail.com>
2024-01-08 17:59:09 +0530

    upadet the app function for to accept docx file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect, url_for, flash
 import PyPDF2
 import io
 from werkzeug.utils import secure_filename
@@ -6,6 +6,8 @@ import re
 from gensim.models.doc2vec import Doc2Vec
 import numpy as np
 from numpy.linalg import norm
+import docx
+
 
 def preprocess_text(text):
     text = text.lower()
@@ -23,7 +25,6 @@ def preprocess_text(text):
 
 
 def model_prediction(input_CV,input_JD):
-    print(input_CV)
     model = Doc2Vec.load('cv_job_maching.model')
     v1 = model.infer_vector(input_CV.split())
     v2 = model.infer_vector(input_JD.split())
@@ -34,6 +35,11 @@ def model_prediction(input_CV,input_JD):
 
 app = Flask(__name__)
 
+def extract_text_from_docx(docx_file):
+    doc = docx.Document(docx_file)
+    text_content = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+    return text_content
+
 def extract_text_from_pdf(pdf_file):
     pdf_content = io.BytesIO(pdf_file.read())
     pdf_reader = PyPDF2.PdfReader(pdf_content)
@@ -42,15 +48,24 @@ def extract_text_from_pdf(pdf_file):
         text_content += page.extract_text()
     return text_content
 
-def extracting_similarity_score(text,pdf_files):
-    first_line_txt = text
-    first_lines = []
-    for pdf_file in pdf_files:
-        pdf_text = extract_text_from_pdf(pdf_file)
-        score = model_prediction(preprocess_text(first_line_txt),preprocess_text(pdf_text))
-        pdf_file_name = secure_filename(pdf_file.filename)
-        first_lines.append({'score': score, 'pdf_file_name': pdf_file_name})
-    return first_lines
+def extracting_similarity_score(text, document_files):
+    final_result = []
+    
+    for document_file in document_files:
+        if document_file.filename.endswith('.pdf'):
+            document_text = extract_text_from_pdf(document_file)
+        elif document_file.filename.endswith('.docx'):
+            document_text = extract_text_from_docx(document_file)
+        else:
+            # Handle unsupported file types or provide an error message
+            flash(f"Unsupported file type: {document_file.filename}", 'error')
+            return redirect(url_for('index'))
+
+        score = model_prediction(preprocess_text(text), preprocess_text(document_text))
+        file_name = document_file.filename
+        final_result.append({'score': score, 'file_name': file_name})
+
+    return final_result
 
 
 @app.route('/')
@@ -73,11 +88,15 @@ def process_data_api():
 @app.route('/process_data_html', methods=['POST'])
 def process_data_html():
     text_data = request.form.get('text_data')
-    pdf_files = request.files.getlist('pdf_file')
-    results = extracting_similarity_score(text_data,pdf_files)
+    document_files = request.files.getlist('document_file')
+    
 
+    results = extracting_similarity_score(text_data, document_files)
+    print(results)
     return render_template('result_multiple.html', results=results)
+ 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
